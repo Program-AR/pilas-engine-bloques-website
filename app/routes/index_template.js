881a4ac6a08304857c511a6de22b9816bc00209d
Hugo Ruscitti <hugoruscitti@gmail.com>
2015-05-23 14:33:39 -0300

    actualizando los links de descargas.

--- app/routes/index_template.js
+++ app/routes/index_template.js
@@ -6,7 +6,7 @@ export default Ember.Route.extend({
       version: "VERSION",
       fecha: "FECHA",
       link_mac: "https://dl.dropboxusercontent.com/u/1335422/releases/pilas-engine-bloques/VERSION/pilas-engine-bloques-VERSION.dmg",
-      link_windows: "https://dl.dropboxusercontent.com/u/1335422/releases/pilas-engine-bloques/VERSION/pilas-engine-bloques_VERSION.exe",
+      link_windows: "https://dl.dropboxusercontent.com/u/1335422/releases/pilas-engine-bloques/VERSION/pilas-engine-bloques-VERSION.exe",
       link_deb: "http://repo.huayra.conectarigualdad.gob.ar/huayra/pool/main/p/pilas-engine-bloques/",
     };
   }
