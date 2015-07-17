786735759a06272a0973a757856ab87fc08f91c2
Hugo Ruscitti <hugoruscitti@gmail.com>
2015-07-17 14:55:17 -0300

    actualizacion...

--- README.md
+++ README.md
@@ -106,3 +106,5 @@ El sitio web de pilas-engine-bloques
 
 
 
+
+
--- app/routes/index.js
+++ app/routes/index.js
@@ -3,10 +3,10 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model: function() {
     return {
-      version: "0.1.11",
-      fecha: "2015-06-24",
-      link_mac: "https://dl.dropboxusercontent.com/u/1335422/releases/pilas-engine-bloques/0.1.11/pilas-engine-bloques-0.1.11.dmg",
-      link_windows: "https://dl.dropboxusercontent.com/u/1335422/releases/pilas-engine-bloques/0.1.11/pilas-engine-bloques-0.1.11.exe",
+      version: "0.4.0",
+      fecha: "2015-07-17",
+      link_mac: "https://dl.dropboxusercontent.com/u/1335422/releases/pilas-engine-bloques/0.4.0/pilas-engine-bloques-0.4.0.dmg",
+      link_windows: "https://dl.dropboxusercontent.com/u/1335422/releases/pilas-engine-bloques/0.4.0/pilas-engine-bloques-0.4.0.exe",
       link_deb: "http://repo.huayra.conectarigualdad.gob.ar/huayra/pool/main/p/pilas-engine-bloques/",
     };
   }
