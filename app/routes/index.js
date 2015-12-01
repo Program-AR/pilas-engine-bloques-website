0b91c7fa7b66ac93c3595f6701984686232d26b5
Hugo Ruscitti <hugoruscitti@gmail.com>
2015-12-01 13:44:43 -0300

    Arreglo menor en la ruta de las descargas.

--- app/routes/index.js
+++ app/routes/index.js
@@ -5,8 +5,8 @@ export default Ember.Route.extend({
     return {
       version: "0.8.7",
       fecha: "2015-09-18",
-      link_mac: "http://static.pilas-engine.com.ar/pilas-engine-bloques/0.9.3/pilas-engine-bloques-0.9.3.dmg",
-      link_windows: "http://static.pilas-engine.com.ar/pilas-engine-bloques/0.9.3/pilas-engine-bloques-0.9.3.exe",
+      link_mac: "http://static.pilas-engine.com.ar/pilas-engine-bloques/0.8.7/pilas-engine-bloques-0.8.7.dmg",
+      link_windows: "http://static.pilas-engine.com.ar/pilas-engine-bloques/0.8.7/pilas-engine-bloques-0.8.7.exe",
       link_deb: "http://repo.huayra.conectarigualdad.gob.ar/huayra/pool/main/p/pilas-engine-bloques/",
     };
   }
