6f7cf6562fe5f91e1776102fa264cd46334c325a
Alf Sanzo <alfredo.sanzo@gmail.com>
2016-03-03 16:52:07 -0300

    release 0.10.0

--- app/routes/index.js
+++ app/routes/index.js
@@ -3,8 +3,8 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model: function() {
     return {
-      version: "0.8.7",
-      fecha: "2015-09-18",
+      version: "0.10.0",
+      fecha: "2016-03-03",
       link_mac: "http://static.pilas-engine.com.ar/pilas-engine-bloques/0.8.7/pilas-engine-bloques-0.8.7.dmg",
       link_windows: "http://static.pilas-engine.com.ar/pilas-engine-bloques/0.8.7/pilas-engine-bloques-0.8.7.exe",
       link_deb: "http://repo.huayra.conectarigualdad.gob.ar/huayra/pool/main/p/pilas-engine-bloques/",
