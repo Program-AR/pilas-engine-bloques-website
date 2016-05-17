0494814ca2f3e70998bbe69b565243161f47159c
Alf Sanzo <alfredo.sanzo@gmail.com>
2016-05-17 18:49:59 -0300

    Release 0.11.1

--- app/routes/index.js
+++ app/routes/index.js
@@ -3,11 +3,11 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model: function() {
     return {
-      version: "0.10.0",
-      fecha: "2016-03-03",
-      link_mac: "binarios/0.10.0/pilasBloques-0.10.0.dmg",
-      link_windows: "binarios/0.10.0/pilasBloques-0.10.0.exe",
-      link_deb: "binarios/0.10.0/pilasBloques-0.10.0.deb",
+      version: "0.11.1",
+      fecha: "2016-05-17",
+      link_mac: "binarios/0.11.1/pilasBloques-0.11.1.dmg",
+      link_windows: "binarios/0.11.1/pilasBloques-0.11.1.exe",
+      link_deb: "binarios/0.11.1/pilasBloques-0.11.1.deb",
     };
   },
 
