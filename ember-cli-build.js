18af97ba1837a0ff03c7461acc18b76e95fe1be7
Hugo Ruscitti <hugoruscitti@gmail.com>
2017-01-21 00:34:35 -0300

    Actualizando la aplicación a ember 2.10.0 closes #242

---
+++ ember-cli-build.js
@@ -0,0 +1,14 @@
+/*jshint node:true*/
+/* global require, module */
+var EmberApp = require('ember-cli/lib/broccoli/ember-app');
+
+module.exports = function(defaults) {
+  var app = new EmberApp(defaults, {
+    // Add options here
+  });
+
+  app.import('vendor/css/bootstrap.css');
+  app.import('vendor/js/bootstrap.min.js');
+
+  return app.toTree();
+};
