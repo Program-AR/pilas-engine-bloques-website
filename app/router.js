be5489416df5ea457e7b3345f7b1b9243654812b
Alf Sanzo <alfredo.sanzo@gmail.com>
2017-01-23 20:46:19 -0300

    Agregada ruta requisitos-minimos

--- app/router.js
+++ app/router.js
@@ -8,6 +8,7 @@ const Router = Ember.Router.extend({
 
 Router.map(function() {
   this.route('demo', {path: "/demo/:actividad"});
+  this.route('requisitos-minimos', {path: "/requisitosMinimos"});
 });
 
 export default Router;
