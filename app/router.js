d87fb6a7da232c5b744c0858c7bb9eaf9a0054e0
Alf Sanzo <alfredo.sanzo@gmail.com>
2017-01-23 21:16:28 -0300

    Agregadas version notes (nueva ruta)

--- app/router.js
+++ app/router.js
@@ -9,6 +9,7 @@ const Router = Ember.Router.extend({
 Router.map(function() {
   this.route('demo', {path: "/demo/:actividad"});
   this.route('requisitos-minimos', {path: "/requisitosMinimos"});
+  this.route('notas-de-version', {path: "/notasDeVersion"});
 });
 
 export default Router;
