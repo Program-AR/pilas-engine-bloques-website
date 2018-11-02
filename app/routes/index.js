a8aec5d24c2e233411c72539132f2e33385fc5de
Alfredo Sanzo <alfredo.sanzo@gmail.com>
2018-11-02 15:53:00 -0300

    Actualizando links de Pilas Bloques a nueva versión

--- app/routes/index.js
+++ app/routes/index.js
@@ -3,12 +3,12 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model: function() {
     return {
-      version: "1.4.0-beta",
-      fecha: "2018-10-12",
-      link_mac: "https://github.com/Program-AR/pilas-bloques/releases/download/1.4.0-beta/pilasbloques-1.4.0-beta.dmg",
-      link_windows: "https://github.com/Program-AR/pilas-bloques/releases/download/1.4.0-beta/pilasbloques-1.4.0-beta.exe",
-      link_ziplinux: "https://github.com/Program-AR/pilas-bloques/releases/download/1.4.0-beta/pilasbloques-1.4.0-beta-linux-x64.zip",
-      link_deb: "https://github.com/Program-AR/pilas-bloques/releases/download/1.4.0-beta/pilasbloques_1.4.0-beta_amd64.deb"
+      version: "1.4.1-beta",
+      fecha: "2018-11-02",
+      link_mac: "https://github.com/Program-AR/pilas-bloques/releases/download/1.4.1-beta/pilasbloques-1.4.1-beta.dmg",
+      link_windows: "https://github.com/Program-AR/pilas-bloques/releases/download/1.4.1-beta/pilasbloques-1.4.1-beta.exe",
+      link_ziplinux: "https://github.com/Program-AR/pilas-bloques/releases/download/1.4.1-beta/pilasbloques-1.4.1-beta-linux-x64.zip",
+      link_deb: "https://github.com/Program-AR/pilas-bloques/releases/download/1.4.1-beta/pilasbloques_1.4.1-beta_amd64.deb"
     };
   },
 
