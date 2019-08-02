595cd1e29d849594ef5a1f6e45f9aeb8f10b5162
Lucas Alan Silvestri <silvestrilucasalan@gmail.com>
2019-08-02 14:55:11 -0300

    update with codemods

--- tests/unit/routes/requisitos-minimos-test.js
+++ tests/unit/routes/requisitos-minimos-test.js
@@ -1,11 +1,11 @@
-import { moduleFor, test } from 'ember-qunit';
+import { module, test } from 'qunit';
+import { setupTest } from 'ember-qunit';
 
-moduleFor('route:requisitos-minimos', 'Unit | Route | requisitos minimos', {
-  // Specify the other units that are required for this test.
-  // needs: ['controller:foo']
-});
+module('Unit | Route | requisitos minimos', function(hooks) {
+  setupTest(hooks);
 
-test('it exists', function(assert) {
-  let route = this.subject();
-  assert.ok(route);
+  test('it exists', function(assert) {
+    let route = this.owner.lookup('route:requisitos-minimos');
+    assert.ok(route);
+  });
 });
