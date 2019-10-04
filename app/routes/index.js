8e30c1079b5c431acee5caa2c1ecbce9de056e61
PalumboN <nahuel.palumbo@gmail.com>
2019-10-04 14:35:43 -0300

    Actualizando links de Pilas Bloques a nueva versión

--- app/routes/index.js
+++ app/routes/index.js
@@ -3,12 +3,12 @@ import Route from '@ember/routing/route';
 export default Route.extend({
   model: function() {
     return {
-      version: "1.5.1",
-      fecha: "2019-09-11",
-      link_mac: "https://github.com/Program-AR/pilas-bloques/releases/download/1.5.1/pilasbloques-1.5.1.dmg",
-      link_windows: "https://github.com/Program-AR/pilas-bloques/releases/download/1.5.1/pilasbloques-1.5.1.exe",
-      link_ziplinux: "https://github.com/Program-AR/pilas-bloques/releases/download/1.5.1/pilasbloques-1.5.1-linux-x64.zip",
-      link_deb: "https://github.com/Program-AR/pilas-bloques/releases/download/1.5.1/pilasbloques_1.5.1_amd64.deb"
+      version: "1.5.2",
+      fecha: "2019-10-04",
+      link_mac: "https://github.com/Program-AR/pilas-bloques/releases/download/1.5.2/pilasbloques-1.5.2.dmg",
+      link_windows: "https://github.com/Program-AR/pilas-bloques/releases/download/1.5.2/pilasbloques-1.5.2.exe",
+      link_ziplinux: "https://github.com/Program-AR/pilas-bloques/releases/download/1.5.2/pilasbloques-1.5.2-linux-x64.zip",
+      link_deb: "https://github.com/Program-AR/pilas-bloques/releases/download/1.5.2/pilasbloques_1.5.2_amd64.deb"
     };
   },
 
