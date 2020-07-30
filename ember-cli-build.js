e6eef9ba98542979b4c6a14e2d0825495e74136c
Lucas Alan Silvestri <silvestrilucasalan@gmail.com>
2020-07-29 22:13:49 -0300

    removed bower, and use npm with font-awesome

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -2,9 +2,16 @@
 
 const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
-module.exports = function(defaults) {
+module.exports = function (defaults) {
   let app = new EmberApp(defaults, {
+
     // Add options here
+
+    'ember-font-awesome': {
+      useScss: false,
+      includeFontFiles: true
+    }
+
   });
 
   app.import('vendor/css/bootstrap.css');
