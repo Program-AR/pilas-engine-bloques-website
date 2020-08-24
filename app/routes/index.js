7ad363253e7e674f83da91d37125738636d33bf3
Lucas Alan Silvestri <silvestrilucasalan@gmail.com>
2020-08-24 16:01:37 -0300

    Actualizando links de Pilas Bloques a nueva versión

--- app/routes/index.js
+++ app/routes/index.js
@@ -3,12 +3,12 @@ import Route from '@ember/routing/route';
 export default Route.extend({
   model: function() {
     return {
-      version: "1.6.0",
-      fecha: "2020-07-22",
-      link_mac: "https://github.com/Program-AR/pilas-bloques/releases/download/1.6.0/pilasbloques-1.6.0.dmg",
-      link_windows: "https://github.com/Program-AR/pilas-bloques/releases/download/1.6.0/pilasbloques-1.6.0.exe",
-      link_ziplinux: "https://github.com/Program-AR/pilas-bloques/releases/download/1.6.0/pilasbloques-1.6.0-linux-x64.zip",
-      link_deb: "https://github.com/Program-AR/pilas-bloques/releases/download/1.6.0/pilasbloques_1.6.0_amd64.deb"
+      version: "1.6.1",
+      fecha: "2020-08-24",
+      link_mac: "https://github.com/Program-AR/pilas-bloques/releases/download/1.6.1/pilasbloques-1.6.1.dmg",
+      link_windows: "https://github.com/Program-AR/pilas-bloques/releases/download/1.6.1/pilasbloques-1.6.1.exe",
+      link_ziplinux: "https://github.com/Program-AR/pilas-bloques/releases/download/1.6.1/pilasbloques-1.6.1-linux-x64.zip",
+      link_deb: "https://github.com/Program-AR/pilas-bloques/releases/download/1.6.1/pilasbloques_1.6.1_amd64.deb"
     };
   },
 
